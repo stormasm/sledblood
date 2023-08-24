c1a6ce006a84bf7a1f90e9512d8f36dbf0c1fbd5
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-08-23 20:33:14 -0700

    use the open function and bypass all of the sled::Config hoops

---
+++ examples/ex02.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /*
+    let mut cfg = sled::Config::default();
+    cfg.cache_capacity_bytes = 256;
+    cfg.path = "sled-names.db".into();
+    let db = cfg.open::<64>().unwrap();
+    */
+
+    let db = sled::open("sled-names.db").expect("failed to open");
+    db.insert("rick", "storm").expect("failed to insert");
+
+    let value = db.get("rick").unwrap().unwrap();
+    println!("{:?}", String::from_utf8_lossy(&value));
+}
