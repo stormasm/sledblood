0680044d4d2d23284f188437f4a97be6fc45d282
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-08-26 09:15:18 -0700

    initial commit of ex04

---
+++ examples/ex04.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let db = sled::open("sled-ex04.db").expect("failed to open");
+
+    db.insert("abc", "defghi").expect("failed to insert 1");
+    db.insert([1, 2, 3], vec![4, 5, 6])
+        .expect("failed to insert 2");
+
+    let value01 = db.get("abc").unwrap().unwrap();
+    let value02 = db.get([1, 2, 3]);
+
+    println!("value01 -> {:?}", String::from_utf8_lossy(&value01));
+    println!("value02 -> {:?}\n", value02.unwrap());
+
+    let mut x = db.iter();
+    //println!("{:?}", x.next());
+
+    while let Some(element) = x.next() {
+        println!("{:?}", element);
+    }
+
+    println!("\nsize_on_disk = {:?}", db.size_on_disk());
+}
